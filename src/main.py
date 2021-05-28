8da293054971ef5e6720ee554bf4c932b4a084a9
ashok <ashok.kumarv789@gmail.com>
2021-05-28 16:28:43 +0530

    it is rel1.1

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 Added new devlopemnt
 add ResourceWarning
 feature1.0
-<<<<<<< HEAD
 
 Iam in master base branch
 
@@ -9,8 +8,6 @@ ima in feature2 of rel1.0
 
 
 base feature 1 in master branch
-=======
 ima in feature2 of rel1.0
 
 base feature2 rel1.0
->>>>>>> rel1.0
